742439404e69c9b2ec8af4de60ea8904b5130dfd
caioagralemos <caioagralemos@live.com>
2023-09-22 15:48:48 -0300

    Greeting App

---
+++ Aula-1/HelloWorld/Greetings.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+var data = new Date()
+var horas = data.getHours()
+var mensagem = ''
+var styles;
+
+if (horas >= 5 && horas < 12) {
+  mensagem = 'Bom dia!'
+} else if (horas >= 12 && horas < 18) {
+  mensagem = 'Boa tarde!'
+} else if (horas >= 18 && horas < 24 || horas >= 0 && horas < 5) {
+  mensagem = 'Boa noite!'
+}
+
+export default function Greetings() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={mensagem == 'Boa noite!' ? {color: '#FFF'} : {color: '#000'}}>{mensagem}</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+if (mensagem == 'Boa noite!') {
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+} else {
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+}
